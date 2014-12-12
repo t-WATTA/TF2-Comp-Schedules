610b8f5abd5bae312ed18fc1de35cfebf0be3647
t-WATTA <twattah@gmail.com>
2014-12-12 16:26:11 +0200

    created netbeans project

---
+++ TF2CompSchedules/src/main/java/schedules/tf2compschedules/App.java
@@ -0,0 +1,13 @@
+package schedules.tf2compschedules;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
